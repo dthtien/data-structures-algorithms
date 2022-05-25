3566e103be4c153b64a95a666655f41a59cc478f
dthtien <tien.dau@employmenthero.com>
2022-05-25 21:09:05 +0700

    Algorithmic toolbox - Week 4: Binary Search

---
+++ algorithmic-toolbox/week4_divide_and_conquer/1_binary_search/binary_search.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+# by dthtien
+
+def binary_search(x, a)
+  low = 0
+  high = a.length - 1
+  while low <= high
+    mid = (low + high) / 2
+    return mid if a[mid] == x
+
+    x < a[mid] ? high = mid - 1 : low = mid + 1
+  end
+
+  -1
+end
+
+if __FILE__ == $0
+  data = STDIN.read.split.map(&:to_i)
+  n = data[0]
+  a = data[1..n]
+  m = data[n + 1]
+  data[n + 2, m].each { |b| print("#{binary_search(b, a)} ") }
+end
