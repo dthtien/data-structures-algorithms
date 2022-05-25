09320b3c61ad3edfb3a588d35570f77bb5cd50fe
dthtien <tien.dau@employmenthero.com>
2022-05-25 21:22:21 +0700

    Algorithmic toolbox - Week 4: Binary Search with duplicated

---
+++ algorithmic-toolbox/week4_divide_and_conquer/2_binary_search-with-duplicated/binary_search_test.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# by dthtien
+require_relative './binary_search'
+require 'test/unit'
+
+class BinarySearch < Test::Unit::TestCase
+  def test_binary_search
+    a = [2, 4, 4, 4, 7, 7, 9]
+    assert_equal(6, binary_search(9, a))
+    assert_equal(1, binary_search(4, a))
+    assert_equal(-1, binary_search(5, a))
+    assert_equal(0, binary_search(2, a))
+  end
+end
